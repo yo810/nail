0a93d101f5df5955441b5657f91884b48406b464
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-12-26 13:38:17 +0000

    add

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,31 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
   def show
     @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to my_page_path, notice: '会員情報の更新が完了しました。'
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def widhdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,6 @@ Rails.application.routes.draw do
   sessions: 'public/sessions'
   }
 
-  #管理者用
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
-
   scope module: :public do
     root 'homes#top'
     get 'customers/my_page' => 'customers#show', as: 'my_page'
@@ -20,6 +15,15 @@ Rails.application.routes.draw do
     get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
     patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
 
+  end
+
+  #管理者用
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
