fb771c2938f322ab15d528f30ff4350045916f71
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2023-01-16 21:04:54 +0000

    add

--- app/controllers/public/reservations_controller.rb
+++ app/controllers/public/reservations_controller.rb
@@ -1,8 +1,22 @@
 class Public::ReservationsController < ApplicationController
   def new
+    @reservation = Reservation.new
+    @day = params[:day]
+    @time = params[:time]
+    @start_time = DateTime.parse(@day + " " + @time + " " + "JST")
   end
 
   def confirm
+    @reservation = Reservation.find(params[:id])
+  end
+  
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to reservation_path @reservation.id
+    else
+      render :new
+    end
   end
 
   private
